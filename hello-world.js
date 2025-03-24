e0a93fd0b774bd3ed10b6bafd5fca6aad081ddd6
junior <zanotti.junior02@gmail.com>
2025-03-24 20:49:24 -0300

    Adicionadas as funcionalidades

--- hello-world.js
+++ hello-world.js
@@ -1,12 +1,37 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
 
+
 nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
         messageP.innerText = '';
     }
-}
+};
+
 
 const helloWorld = () => {
-    messageP.innerText = `Hello World, ${nameInput.value}!`;
-}
+    messageP.innerText = nameInput.value.trim() 
+        ? `Hello World, ${nameInput.value}!` 
+        : "Hello World!";
+};
+
+
+nameInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        helloWorld();
+    }
+});
+
+
+nameInput.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        nameInput.value = "";
+        messageP.innerText = "";
+    }
+});
+
+
+const limparCampos = () => {
+    nameInput.value = "";
+    messageP.innerText = "";
+};
